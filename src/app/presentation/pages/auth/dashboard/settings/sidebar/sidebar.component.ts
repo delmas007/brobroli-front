874e6615383f9e43a11464756a061f05321815bd
fatimtraore <fatytraore997@gmail.com>
2024-11-08 18:45:24 +0100

    6 adjustment of the css of the service provider dashboard

--- src/app/presentation/pages/auth/dashboard/settings/sidebar/sidebar.component.ts
+++ src/app/presentation/pages/auth/dashboard/settings/sidebar/sidebar.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, RouterLink, RouterOutlet} from "@angular/router";
+import {ActivatedRoute, RouterModule, RouterOutlet} from "@angular/router";
 import {MatIcon} from "@angular/material/icon";
 import {Person} from "@interfaces/person";
 import {User} from "@interfaces/user";
@@ -10,7 +10,7 @@ import {NgIf} from "@angular/common";
   selector: 'app-sidebar',
   standalone: true,
   imports: [
-    RouterLink,
+    RouterModule,
     MatIcon,
     RouterOutlet,
     NgIf
